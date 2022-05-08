4935cc65c54e9dc07a75e304105bd6b41c45754d
unknown <kamilolech07@gmail.com>
2022-05-08 19:04:03 +0100

    sending to db complete

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,15 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
+	@Autowired
+	private repo myRepo;
 	
 	@GetMapping("")
-	public String showHome()
+	public String showHome(Model model)
 	{
+		model.addAttribute("car", new Car());
 		return "index";
 	}
+	@PostMapping("")
+	public String sendToDb(Car car)
+	
+	{
+		myRepo.save(car);
+		return "viewThem";
+		
+	}
+	
 
 }
