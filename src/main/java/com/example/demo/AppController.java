45eeedde2b3bde4fe93dac19b2258d00f768780a
unknown <kamilolech07@gmail.com>
2022-05-13 17:43:08 +0100

    link to next page working

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 
@@ -42,6 +43,12 @@ public class AppController {
 	public String showTested()
 	{
 		return "tested";
+	}
+	
+	@GetMapping("/tested/{id}")
+	public Car show(@PathVariable Long id)
+	{
+		return myRepo.getById(id);
 	}
 	
 
